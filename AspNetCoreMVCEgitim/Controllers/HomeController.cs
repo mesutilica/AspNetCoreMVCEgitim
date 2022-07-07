b0bf15c317c2f13c4369dbcc15182ab64f9cbe60
Mesut ILICA <mesut.ilica@gmail.com>
2022-07-07 13:00:59 +0300

    07-07-2022 App settings

--- AspNetCoreMVCEgitim/Controllers/HomeController.cs
+++ AspNetCoreMVCEgitim/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using AspNetCoreMVCEgitim.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -8,37 +9,35 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace AspNetCoreMVCEgitim.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
             HttpContext.Session.SetString("deger", "deneme"); // AspNetCore.Http; usingi ekle
-
             return View();
         }
 
         public IActionResult Privacy()
         {
             TempData["data"] = HttpContext.Session.GetString("deger");
-
             return View();
         }
 
         public IActionResult SessionSil()
         {
             HttpContext.Session.Remove("deger");
-
             // kullanıcıya ait, tüm session’ları siler.
-
             //HttpContext.Session.Clear();
             return RedirectToAction(nameof(Privacy));
         }
@@ -51,28 +50,32 @@ namespace AspNetCoreMVCEgitim.Controllers
             };
             Response.Cookies.Append("username", "admin", cookie);
             Response.Cookies.Append("password", "123456", cookie);
-
             return RedirectToAction(nameof(CookieOku));
         }
 
         public IActionResult CookieOku()
         {
-
             TempData["kullanici"] = Request.Cookies["username"];
             TempData["parola"] = Request.Cookies["password"];
-
             return View();
-
         }
 
         public IActionResult CookieSil()
         {
             Response.Cookies.Delete("username");
             Response.Cookies.Delete("password");
-
             return RedirectToAction(nameof(CookieOku));
-
         }
+
+        public IActionResult AppSetting()
+        {
+            TempData["Email"] = _configuration["Email"];
+            TempData["MailSunucu"] = _configuration["MailSunucu"];
+            TempData["UserName"] = _configuration["MailKullanici:UserName"];
+            TempData["Password"] = _configuration.GetSection("MailKullanici:UserName").Value;
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
